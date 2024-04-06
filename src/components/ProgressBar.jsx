8aa24e6929bc397ffccda862d9e83df0ebc4a6ff
Tanveer <shaikhtanveer5205@gmail.com>
2024-04-06 11:45:01 +0530

    progress bar

---
+++ src/components/ProgressBar.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function ProgressBar() {
+  return (
+    <div className="w-full bg-gray-200 h-2.5">
+      <div className="bg-purple-600 h-2.5 rounded-md w-5/6"></div>
+    </div>
+  );
+}
