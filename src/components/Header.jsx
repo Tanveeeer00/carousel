6acaa4e2f5a5345677049e4f4c2a7760234b156c
Tanveer <shaikhtanveer5205@gmail.com>
2024-04-06 11:46:12 +0530

    title

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Header({ text, className, num, Bg, children }) {
+  return (
+    <div className={`flex justify-between px-4 py-1 ${Bg}`}>
+      <div className={`flex justify-center items-center gap-1`}>
+        <span className={` text-white text-2xl`}>{children}</span>
+        <h1 className={`text-xl font-extrabold ${className}`}>{text}</h1>
+      </div>
+      <h1 className={`text-sm font-bold ${className}`}>{num}</h1>
+    </div>
+  );
+}
